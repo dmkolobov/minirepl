9ccc1ff574a1ec8a4f0b851d8ce4f79a35512d87
Dmitry Kolobov <dmkolobov@gmail.com>
2014-12-28 00:47:31 -0600

    Update to om 0.8.0-beta5

--- project.clj
+++ project.clj
@@ -17,7 +17,7 @@
                  [org.clojure/clojurescript "0.0-2371"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.clojure/clojure "1.6.0"]
-                 [om "0.8.0-alpha2"]
+                 [om "0.8.0-beta5"]
                  [environ "1.0.0"]
                  [ring-server "0.3.1"]
                  [secretary "1.2.1"]]
